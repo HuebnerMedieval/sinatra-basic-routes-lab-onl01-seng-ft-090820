4d6918c802e857f0c2d7eb5830fdb30d7bd6b02a
elimhuebner@gmail.com <ehbner@DESKTOP-O6P2HM5.localdomain>
2020-10-14 10:23:33 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Thorstein"
+    end
+
+    get '/hometown' do
+        "My hometown is Borg"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Os Sete Does"
+    end
 end
